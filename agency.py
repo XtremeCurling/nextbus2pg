55539adcff36bd1efb2080ab6fe4c036899000c6
Robert Sharp <bob.wynbert.sharp@gmail.com>
2018-03-12 15:36:15 -0700

    Update the route table for a particular agency

--- agency.py
+++ agency.py
@@ -1,6 +1,7 @@
 from pyquery import PyQuery as pq
 import psycopg2
 import psycopg2.extras
+import uuid
 
 # Get the current "agencyList" from the nextbus API. Upsert to the postgres database.
 def update_agency(conn):
@@ -25,4 +26,32 @@ def update_agency(conn):
 	with cur as conn.cursor():
 		psycopg2.extras.execute_values(
 			cur, upsert_sql, agency_rows
+		)
+
+# Get an agency's current "routeList" from the nextbus API. Upsert to the postgres database.
+def update_route(conn, agency_id):
+	# Hit the routeList endpoint.
+	route_pq = pq(
+		url = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a={0}'.format(agency_id)
+	)
+	# Format the results as a list of tuples for psycopg2.
+	route_rows = [(
+		uuid.uuid4(),
+		agency_id,
+		i.attr('tag'),
+		i.attr('title')
+	) for i in route_pq.items('route')]
+	# Create the UPSERT command.
+	# If route is already in database, update its name.
+	upsert_sql = '''
+		INSERT INTO nextbus.route (route_id, agency_id, tag, name)
+			VALUES %s
+			ON CONFLICT (agency_id, tag)
+			DO UPDATE SET
+				(name) = (EXCLUDED.name)
+	'''
+	# Execute the UPSERT command.
+	with cur as conn.cursor()
+		psycopg2.extras.execute_values(
+			cur, upsert_sql, route_rows
 		)
