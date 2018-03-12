927e55a04c7929a7fb3e038e3e96d3dab85d0698
Robert Sharp <bob.wynbert.sharp@gmail.com>
2018-03-12 15:23:44 -0700

    Connect to the postgres database

---
+++ connect.py
@@ -0,0 +1,12 @@
+import urllib
+import psycopg2
+import psycopg2.extras
+
+# Connect to a postgres database. Tweak some things.
+def pgconnect(pghost, pgdb, pguser):
+	connection = psycopg2.connect(host = pghost, dbname = pgdb, user = pguser)
+	# Set autocommit to avoid repetitive connection.commit() statements.
+	connection.autocommit = True
+	# Register the UUID adapter globally.
+	psycopg2.extras.register_uuid()
+	return connection
