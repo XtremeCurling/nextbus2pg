93f615b1bd5057b9f6451449c5d843c0dc524a96
Robert Sharp <bob.wynbert.sharp@gmail.com>
2018-04-23 20:11:46 -0700

    Create unique index for vehicle location records

--- sql/create_vehicle_time_index.sql
+++ sql/create_vehicle_time_index.sql
@@ -9,3 +9,10 @@ DELETE FROM nextbus.vehicle_location vl1
         AND vl1.vehicle_tag = vl2.vehicle_tag
         AND vl1.location_timestamp = vl2.location_timestamp
         AND vl1.ctid < vl2.ctid; -- Keep only the most recent record
+
+/*
+Create uniqueness index on the set of fields (service_id, vehicle_tag,
+location_timestamp)
+*/
+CREATE UNIQUE INDEX location_defined_by_service_vehicle_timestamp_idx
+    ON nextbus.vehicle_location (service_id, vehicle_tag, location_timestamp);
